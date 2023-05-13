3b1942f717d1572dd1d47f1095d5efdcd2d9c8d7
Ivan Shamilov <shamilov.ivan21@gmail.com>
2023-05-13 18:47:34 +0200

    generator added

--- model/model.py
+++ model/model.py
@@ -14,34 +14,52 @@ from analysis.helpers import *
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 batch_size = 64
-
-
+leaky_relu_slope = 0.05
 block_size = 16  # -> window size for the dataset (length of the context)
 latent_dim = 100 # -> size of the latent space
 embedding_dim = 96 # 192 -> embedding dimension for the condition
-ca_num_heads = 6 # -> number of heads in Cross-Attention block - each head with a size of embedding_dim // num_heads = 192 / 6 = 32
-ca_dropout = 0.15 # -> dropout for Cross-Attention block
-ffn_dropout = 0.1 # -> dropout for feed-forward networks
+n_attention_blocks = 3 # -> number of consecutive Self-Attention blocks
 sa_num_heads = 6  # -> head size in Self-Attention blocks
+ca_num_heads = 6 # -> number of heads in Cross-Attention block - each head with a size of embedding_dim // num_heads = 192 / 6 = 32
+attn_dropout = 0.15 # -> dropout for Attention blocks
+ffn_dropout = 0.1 # -> dropout for feed-forward networks
 fc_cond_inner = 128 # -> size of inner layer of condition embedding
 inner_mapping = Mapper().inner_mapping   # -> used to convert the real ASCII key symbols to range [0; 100] 
 vocab_size = max(inner_mapping.values()) + 1 # -> to be used in ks_embedding_table, equals to 100
+generator_output_dim = 4
+
+#================ Feed Forward Network ================#
+
+class FeedForward(nn.Module):
+  def __init__(self, inner_dim: int, num_inner_layers: int = 0, in_dim: int = embedding_dim, out_dim: int = embedding_dim):
+    super(FeedForward, self).__init__()
+    layers = [
+      nn.Linear(in_features=in_dim, out_features=inner_dim),
+      nn.LeakyReLU(leaky_relu_slope, inplace=True),
+    ]
+    for _ in range(num_inner_layers):
+      layers.append(nn.Linear(in_features=inner_dim, out_features=inner_dim))
+      layers.append(nn.LeakyReLU(leaky_relu_slope, inplace=True))
+      layers.append(nn.Dropout(ffn_dropout))
+
+    layers.append(nn.Linear(in_features=inner_dim, out_features=out_dim))
+    layers.append(nn.Dropout(ffn_dropout))
+    
+    self.ffn = nn.Sequential(*layers)
+
+  def forward(self, x):
+    return self.ffn(x)
 
 #================ Condition Embedding ================#
 
 class ConditionEmbedding(nn.Module):
-  def __init__(self, embedding_dim, inner_dim):
+  def __init__(self, embedding_dim: int, inner_dim: int):
     super(ConditionEmbedding, self).__init__()
     # Embed the condition's keystrokes and position
     self.ks_embedding_table_cond = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embedding_dim)
     self.position_embedding_table_cond = nn.Embedding(num_embeddings=block_size, embedding_dim=embedding_dim)
     # Linear projection
-    self.ffn = nn.Sequential(
-      nn.Linear(in_features=embedding_dim, out_features=inner_dim),
-      nn.ReLU(True),
-      nn.Linear(in_features=inner_dim, out_features=embedding_dim),
-      nn.Dropout(ffn_dropout)
-    )
+    self.ffn = FeedForward(inner_dim=inner_dim)
   
   def forward(self, x):
     batch_dim, time_dim = x.shape
@@ -51,6 +69,23 @@ class ConditionEmbedding(nn.Module):
     x = self.ffn(x) # (batch_dim, time_dim, embedding_dim)
     return x
 
+#================ Self-Attention Block ===============#
+
+class AttentionBlock(nn.Module):
+  def __init__(self, fc_inner_dim: int):
+    super(AttentionBlock, self).__init__()
+    self.sa_block = nn.MultiheadAttention(embed_dim=embedding_dim, num_heads=sa_num_heads, dropout=attn_dropout)
+    self.ln1 = nn.LayerNorm(embedding_dim)
+    self.ffn = FeedForward(inner_dim=fc_inner_dim)
+    self.ln2 = nn.LayerNorm(embedding_dim)
+
+  def forward(self, x):
+    x = self.ln1(x)
+    x_attn, _ = self.sa_block(query=x, key=x, value=x)
+    x += x_attn
+    x += self.ffn(self.ln2(x))
+    return x
+
 #================ Generator ================#
 
 class Generator(nn.Module):
@@ -58,7 +93,11 @@ class Generator(nn.Module):
     super(Generator, self).__init__()
     self.condition_embd = ConditionEmbedding(embedding_dim, fc_cond_inner)
     self.latent_fc = nn.Linear(in_features=latent_dim, out_features=block_size * embedding_dim)
-    self.cross_attention = nn.MultiheadAttention(embed_dim=embedding_dim, num_heads=ca_num_heads, dropout=ca_dropout)
+    self.l_relu = nn.LeakyReLU(leaky_relu_slope, inplace=True)
+    self.cross_attention = nn.MultiheadAttention(embed_dim=embedding_dim, num_heads=ca_num_heads, dropout=attn_dropout)
+    self.sa_blocks = nn.Sequential(*[AttentionBlock(fc_inner_dim=2*embedding_dim) for _ in range(n_attention_blocks)])
+    self.ln = nn.LayerNorm(embedding_dim)
+    self.ffn_head = FeedForward(inner_dim=128, out_dim=generator_output_dim)
 
   def forward(self, latent_space, x):
     batch_dim, latent_dim = latent_space.shape
@@ -66,12 +105,13 @@ class Generator(nn.Module):
     cond = self.condition_embd(x)  # (batch_dim, time_dim, embedding_dim)
     print(f"Condition data shape: {cond.shape}")
     # Project the latent space to get an appropriate shape 
-    latent = self.latent_fc(latent_space)  # (batch_dim, embedding_dim)
-    latent = latent.view(batch_dim, block_size, embedding_dim)
-    latent = F.leaky_relu(latent, 0.2)
+    latent = self.l_relu(self.latent_fc(latent_space))  # (batch_dim, embedding_dim)
+    latent = latent.view(batch_dim, block_size, embedding_dim)  # (batch_dim, time_dim, embedding_dim)
     print(f"Latent space shape: {latent.shape}")
     # Cross-Attention: condition (key, value), latent space (query)
-    x, _ = self.cross_attention(query=latent, value=cond, key=cond)
+    x, _ = self.cross_attention(query=latent, value=cond, key=cond)  # (batch_dim, time_dim, embedding_dim)
+    x = self.sa_blocks(x)  # (batch_dim, time_dim, embedding_dim)
+    x = self.ffn_head(self.ln(x))  # (batch_dim, time_dim, 4)
 
     return x
   
@@ -82,5 +122,6 @@ if __name__ == "__main__":
 
   for i, (ks, ks_time) in enumerate(dataloader):
     latent_space = torch.randn(ks.shape[0], latent_dim, device=device)
-    print(f"Output shape: {generator(latent_space, ks).shape}")
+    out = generator(latent_space, ks)
+    print(out.shape)
     break
