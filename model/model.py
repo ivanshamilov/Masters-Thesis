a5ab6c5ac48b3a5ac12c4d61c5a9e2942aeba7f8
Ivan Shamilov <shamilov.ivan21@gmail.com>
2023-05-14 12:21:42 +0200

    Discriminator added

--- model/model.py
+++ model/model.py
@@ -18,7 +18,8 @@ leaky_relu_slope = 0.05
 block_size = 16  # -> window size for the dataset (length of the context)
 latent_dim = 100 # -> size of the latent space
 embedding_dim = 96 # 192 -> embedding dimension for the condition
-n_attention_blocks = 3 # -> number of consecutive Self-Attention blocks
+n_attention_blocks_gen = 3 # -> number of consecutive Self-Attention blocks in generator
+n_attention_blocks_dis = 1 # -> number of consecutive Self-Attention blocks in discriminator
 sa_num_heads = 6  # -> head size in Self-Attention blocks
 ca_num_heads = 6 # -> number of heads in Cross-Attention block - each head with a size of embedding_dim // num_heads = 192 / 6 = 32
 attn_dropout = 0.15 # -> dropout for Attention blocks
@@ -95,7 +96,7 @@ class Generator(nn.Module):
     self.latent_fc = nn.Linear(in_features=latent_dim, out_features=block_size * embedding_dim)
     self.l_relu = nn.LeakyReLU(leaky_relu_slope, inplace=True)
     self.cross_attention = nn.MultiheadAttention(embed_dim=embedding_dim, num_heads=ca_num_heads, dropout=attn_dropout)
-    self.sa_blocks = nn.Sequential(*[AttentionBlock(fc_inner_dim=2*embedding_dim) for _ in range(n_attention_blocks)])
+    self.sa_blocks = nn.Sequential(*[AttentionBlock(fc_inner_dim=2*embedding_dim) for _ in range(n_attention_blocks_gen)])
     self.ln = nn.LayerNorm(embedding_dim)
     self.ffn_head = FeedForward(inner_dim=128, out_dim=generator_output_dim)
 
@@ -103,11 +104,9 @@ class Generator(nn.Module):
     batch_dim, latent_dim = latent_space.shape
     # Embed the condition
     cond = self.condition_embd(x)  # (batch_dim, time_dim, embedding_dim)
-    print(f"Condition data shape: {cond.shape}")
     # Project the latent space to get an appropriate shape 
     latent = self.l_relu(self.latent_fc(latent_space))  # (batch_dim, embedding_dim)
     latent = latent.view(batch_dim, block_size, embedding_dim)  # (batch_dim, time_dim, embedding_dim)
-    print(f"Latent space shape: {latent.shape}")
     # Cross-Attention: condition (key, value), latent space (query)
     x, _ = self.cross_attention(query=latent, value=cond, key=cond)  # (batch_dim, time_dim, embedding_dim)
     x = self.sa_blocks(x)  # (batch_dim, time_dim, embedding_dim)
@@ -115,13 +114,39 @@ class Generator(nn.Module):
 
     return x
   
+#================ Discriminator ================#
 
+class Discriminator(nn.Module):
+  def __init__(self):
+    super(Discriminator, self).__init__()
+    self.condition_embd = ConditionEmbedding(embedding_dim, fc_cond_inner)
+    self.latent_fc = nn.Linear(in_features=generator_output_dim, out_features=embedding_dim)
+    self.l_relu = nn.LeakyReLU(leaky_relu_slope, inplace=True)
+    self.cross_attention = nn.MultiheadAttention(embed_dim=embedding_dim, num_heads=ca_num_heads, dropout=attn_dropout)
+    self.sa_blocks = nn.Sequential(*[AttentionBlock(fc_inner_dim=4*embedding_dim) for _ in range(n_attention_blocks_dis)])
+    self.ffn = FeedForward(inner_dim=4 * embedding_dim, num_inner_layers=1)
+    self.ln1 = nn.LayerNorm(embedding_dim)
+    self.disc_head = nn.Linear(in_features=embedding_dim, out_features=1)
+    self.sigmoid = nn.Sigmoid()
+
+  def forward(self, keystrokes, condition_symbols):
+    batch_dim, latent_dim = latent_space.shape
+    cond = self.condition_embd(condition_symbols)  # (batch_dim, time_dim, embedding_dim)
+    latent = self.l_relu(self.latent_fc(keystrokes))  # (batch_dim, time_dim, embedding_dim)
+    x, _ = self.cross_attention(query=latent, value=cond, key=cond)  # (batch_dim, time_dim, embedding_dim)
+    x = self.sa_blocks(x) # (batch_dim, time_dim, embedding_dim)
+    x = self.ln1(self.ffn(x)) # (batch_dim, time_dim, embedding_dim)
+    x = self.sigmoid(self.disc_head(x)) # (batch_dim, time_dim, 1)
+    return x
+
+  
 if __name__ == "__main__":
   dataloader = create_dataloader(path=MAIN_DIR, window_size=block_size, batch_size=batch_size, shuffle=True, limit=1)
   generator = Generator()
-
+  discriminator = Discriminator()
   for i, (ks, ks_time) in enumerate(dataloader):
     latent_space = torch.randn(ks.shape[0], latent_dim, device=device)
-    out = generator(latent_space, ks)
+    # out = generator(latent_space, ks)
+    out = discriminator(ks_time, ks)
     print(out.shape)
     break
